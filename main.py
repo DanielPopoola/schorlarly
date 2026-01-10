a278297c01df18b159b6e34f7277d6b2c47e0ba7
Daniel Popoola <iamuchihadaniel236@gmail.com>
2026-01-10 10:53:47 +0100

    refactor: starting afresh

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from schorlarly!")
-
-
-if __name__ == "__main__":
-    main()
--- src/storage/helpers.py
+++
@@ -1,9 +0,0 @@
-import json
-from enum import Enum
-
-
-class EnumEncoder(json.JSONEncoder):
-	def default(self, o):
-		if isinstance(o, Enum):
-			return o.value
-		return super().default(o)
